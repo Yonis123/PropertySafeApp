b99e70b871b5786262b0e2a769b582e61d214c97
Yonis Nur <ynur472@gmail.com>
2024-09-06 21:51:34 -0400

    fix PostgreSQL setup for Heroku

--- app/config.py
+++ app/config.py
@@ -2,18 +2,20 @@ import os
 
 
 
+import os
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') 
-    
-    MAIL_SERVER = 'smtp.gmail.com'  # Replace with your SMTP server
-    MAIL_PORT = 587  # Typically 587 for TLS or 465 for SSL
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
-    CORS_HEADERS='Content-Type'
-    SESSION_TYPE='filesystem'
+    CORS_HEADERS = 'Content-Type'
+    SESSION_TYPE = 'filesystem'
