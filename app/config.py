66cdb3492ff25a57448c3fe01de565e3dcdae50a
Yonis Nur <ynur472@gmail.com>
2024-09-06 22:01:05 -0400

    added some stuff

--- app/config.py
+++ app/config.py
@@ -1,15 +1,10 @@
 import os
 
-
-
-import os
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
