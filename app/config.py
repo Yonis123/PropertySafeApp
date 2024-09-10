3d8be1781efeaf9671c4d6a9daec5769811e6318
Yonis Nur <ynur472@gmail.com>
2024-09-10 14:35:58 -0400

    changed config file a lot

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    SECRET_KEY = os.getenv('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') 
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY') 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -14,3 +14,8 @@ class Config:
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
     CORS_HEADERS = 'Content-Type'
     SESSION_TYPE = 'filesystem'
+    REACT_APP_API_URL=os.getenv('REACT_APP_API_URL')
+    LOGIN_URL=os.getenv('LOGIN_URL')
+    API_URL=os.getenv('API_URL')
+    OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
+    FLASK_APP=os.getenv('FLASK_APP')
